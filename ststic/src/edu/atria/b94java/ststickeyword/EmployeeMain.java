2a1646d8c1daee4b9b0536f21a9e1966d7a4965a
Asus <Asus@LAPTOP-SH6AB4T9>
2024-06-13 09:24:02 +0530

    second commit

---
+++ ststic/src/edu/atria/b94java/ststickeyword/EmployeeMain.java
@@ -0,0 +1,14 @@
+package edu.atria.b94java.ststickeyword;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee eOne=new Employee(101,"rohit");
+		System.out.println("The Empolyee eOne Details are:"+eOne);
+		
+		Employee eTwo= new Employee(111, "Ranjith");
+		System.out.println("The Empolyee eTwo Details are:"+eTwo);
+	}
+
+}
